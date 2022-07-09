55617479d3f2a163bddb81aa900f39bb3f6c510d
Alex Conder <alex@kenzie.com.br>
2022-07-09 08:34:17 -0300

    feat: creating login and autologin routes + importing controllers

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { USER_REGISTER } from "../controllers/user";
+import { USER_AUTOLOGIN, USER_LOGIN, USER_REGISTER } from "../controllers/user";
 
 const router: Router = Router();
 
 router.post('/', USER_REGISTER);
+router.post('/login', USER_LOGIN);
+router.post('/autologin', USER_AUTOLOGIN);
 
 export default router;
