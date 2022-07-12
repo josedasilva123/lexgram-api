238a60cd05a23fbe2f42796da8bf753ac13a028e
Alex Conder <alex.conder@nerdweb.com.br>
2022-07-11 21:22:21 -0300

    feat: postCreate, getFollowersPosts and getUserPosts controllers created

---
+++ src/controllers/post.ts
@@ -0,0 +1,149 @@
+import { Request, response, Response } from "express";
+
+import { deleteServerFile, uploadFile } from "../functions/googledrive/fileupload";
+import sharp from "sharp";
+
+import User, { iFollower } from "../models/user";
+import Post from "../models/post";
+import { ObjectId } from "mongodb";
+
+export const POST_CREATE = async (req: Request, res: Response) => {
+  try {
+    const { userID, description } = req.body;
+
+    const file = req.file;
+
+    if (!userID || !description) {
+      throw new Error(
+        "Parece que algum parâmetro obrigatório do body está faltando."
+      );
+    }
+
+    if (!file) {
+      throw new Error("Arquivo enviando inválido.");
+    }
+
+    await sharp(file.path)
+      .resize({
+        fit: sharp.fit.contain,
+        width: 1000,
+      })
+      .webp({ quality: 50 })
+      .toFile(`uploads/webp/${file.filename}`);
+
+    const compressedFile = { ...file, path: "uploads\\webp\\" + file.filename };
+
+    deleteServerFile(file.path);
+
+    const upload: any = await uploadFile(compressedFile);
+
+    deleteServerFile(compressedFile.path);
+
+    if (!upload) {
+      throw new Error("Desculpe! Não foi possível realizar o upload!");
+    }
+
+    const currentDate = new Date();
+
+    const newPost = {
+      userID,
+      image: upload.data.image,
+      description,
+      createdAt: currentDate,
+      updatedAt: currentDate,
+    };
+
+    const response = await Post.create(newPost);
+
+    res
+      .status(200)
+      .json({ message: "Post criado com sucesso!", post: response });
+  } catch (error: any) {
+    response.status(400).json({ error: error.message });
+  }
+};
+
+export const FOLLOWERS_POSTS_GET = async (req: Request, res: Response) => {
+  try {
+    const { user, page, perPage } = req.query;
+
+    if (!user || !page || !perPage) {
+      throw new Error(
+        "Parece que algum parâmetro obrigatório da query está faltando."
+      );
+    }
+
+    const skip = Number(page) * Number(perPage);
+
+    const objectUserID = new ObjectId(String(user));
+
+    const currentUser: any = await User.find({ _id: objectUserID });
+
+    if (!currentUser) {
+      throw new Error("O usuário fornecido é inválido!");
+    }
+
+    const followersID = currentUser.followers.map((follower: iFollower) => {
+      return follower.userID;
+    });
+
+    const query = {
+      userID: { $in: [...followersID] },
+    };
+
+    const count = await Post.find(query).count();
+
+    const formula = skip + Number(perPage);
+
+    let next;
+
+    if(count > formula){
+      next = `?user=${user}&page=${Number(page) + 1}&perPage=${perPage}&`;
+    } else {
+      next = false;
+    }
+
+    const response = await Post.find(query).skip(skip).limit(Number(perPage));
+
+    res.status(200).json({ count, next, posts: response });
+  } catch (error: any) {
+    response.status(400).json({ error: error.message });
+  }
+};
+
+
+export const USER_POSTS_GET = async (req: Request, res: Response) => {
+  try {
+    const { user, page, perPage } = req.query;
+
+    if (!user || !page || !perPage) {
+      throw new Error(
+        "Parece que algum parâmetro obrigatório da query está faltando."
+      );
+    }
+
+    const skip = Number(page) * Number(perPage);
+
+    const query = {
+      userID: user,
+    };
+
+    const count = await Post.find(query).count();
+
+    const formula = skip + Number(perPage);
+
+    let next;
+
+    if(count > formula){
+      next = `?user=${user}&page=${Number(page) + 1}&perPage=${perPage}&`;
+    } else {
+      next = false;
+    }
+
+    const response = await Post.find(query).skip(skip).limit(Number(perPage));
+
+    res.status(200).json({ count, next, posts: response });
+  } catch (error: any) {
+    response.status(400).json({ error: error.message });
+  }
+};
