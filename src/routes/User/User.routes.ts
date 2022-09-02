bc0ea66c386bbdcc2c874c48fa1d6f9096876813
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2022-09-01 21:21:13 -0300

    feat: applying handle errors in user routes

--- src/routes/User/User.routes.ts
+++ src/routes/User/User.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { Authenticate } from "../../middlewares/authenticate";
+import { HandleErrors } from "../../middlewares/handleErrors";
 import { Validate } from "../../middlewares/handleValidation";
 import {
   userLoginValidation,
@@ -9,9 +10,9 @@ import UserControllers from "./UserControllers";
 
 const router: Router = Router();
 
-router.post("/", userRegisterValidation(), Validate, UserControllers.Register);
-router.post("/login", userLoginValidation(), Validate, UserControllers.Login);
-router.post("/autologin", Authenticate, UserControllers.AutoLogin);
-router.get("/verify/:slug", UserControllers.AutoLogin);
+router.post("/", userRegisterValidation(), Validate, HandleErrors(UserControllers.Register));
+router.post("/login", userLoginValidation(), Validate, HandleErrors(UserControllers.Login));
+router.post("/autologin", Authenticate, HandleErrors(UserControllers.AutoLogin));
+router.get("/verify/:slug", HandleErrors(UserControllers.AutoLogin));
 
 export default router;
--- src/routes/User/UserControllers.ts
+++ src/routes/User/UserControllers.ts
@@ -19,47 +19,31 @@ export default class UserControllers {
     req: Request<{}, {}, iRegisterBody, {}>,
     res: Response<iRegisterSucessResponse | iErrorResponse>
   ) {
-    try {
-      const response = await UserServices.Register(req.body);
-      return response;
-    } catch (error) {
-      res.status(400).json({ error: (error as Error).message });
-    }
+    const response = await UserServices.Register(req.body);
+    res.status(200).json(response);
   }
 
   static async Login(
     req: Request<{}, {}, iLoginBody, {}>,
     res: Response<iLoginSucessResponse | iErrorResponse>
   ) {
-    try {
-      const response = await UserServices.Login(req.body);
-      return response;
-    } catch (error) {
-      res.status(400).json({ error: (error as Error).message });
-    }
+    const response = await UserServices.Login(req.body);
+    res.status(200).json(response);
   }
 
   static async AutoLogin(
     req: Request<{}, {}, iAutoLoginBody, {}>,
     res: Response<iLoginSucessResponse | iErrorResponse>
   ) {
-    try {
-      const response = await UserServices.AutoLogin(req.body);
-      res.status(200).json(response);
-    } catch (error) {
-      res.status(400).json({ error: (error as Error).message });
-    }
+    const response = await UserServices.AutoLogin(req.body);
+    res.status(200).json(response);
   }
 
   static async VerifySlug(
     req: Request<{}, {}, {}, iVerifySlugQuery>,
     res: Response<iVerifySlugSucessResponse | iErrorResponse>
   ) {
-    try {
-      const response = await UserServices.VerifySlug(req.query);
-      res.status(200).json(response);
-    } catch (error) {
-      res.status(400).json({ error: (error as Error).message });
-    }
+    const response = await UserServices.VerifySlug(req.query);
+    res.status(200).json(response);
   }
 }
