868e0dcd94e75b12437fd7bcc66693e758b5e1d2
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2022-09-03 08:11:08 -0300

    feat: creating password token authentication

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -1,18 +1,48 @@
 import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
-export const Authenticate = (req: Request, res: Response, next: NextFunction) => {
-  const {auth} = req.headers;  
+type tJwtVerify = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+  token: string | string[],
+  secretKey: string
+) => void;
+
+const JwtVerify: tJwtVerify = (req, res, next, token, secretKey) => {
+  jwt.verify(String(token), secretKey, (err, decoded: any) => {
+    if (!err) {
+      req.body.decodedID = decoded.id;
+      next();
+    } else {
+      res.status(400).json({ error: "Sua token expirou ou é inválida!" });
+      return false;
+    }
+  });
+};
+
+export const Authenticate = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { auth } = req.headers;
   const secretKey = process.env.JWT_SECRET_KEY;
 
-  if(auth && secretKey){
-    jwt.verify(String(auth), secretKey, (err, decoded: any) => {
-        if(!err){
-            req.body.decodedID = decoded.id;
-            next();
-        } else {
-            res.status(400).json({ error: 'Sua token expirou ou é inválida!'})
-        }
-    })
+  if (auth && secretKey) {
+    JwtVerify(req, res, next, auth, secretKey);
   }
-}
+};
+
+export const AuthenticatePassword = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { auth } = req.headers;
+  const secretKey = process.env.JWT_SECRET_RECOVER;
+
+  if (auth && secretKey) {
+    JwtVerify(req, res, next, auth, secretKey);
+  }
+};
