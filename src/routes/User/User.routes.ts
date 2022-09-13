13f695a55159e37737d49954b3664491b0f27df2
Alex Conder <55949988+josedasilva123@users.noreply.github.com>
2022-09-13 08:35:13 -0300

    fix: get route does not need validation

--- src/routes/User/User.routes.ts
+++ src/routes/User/User.routes.ts
@@ -15,7 +15,7 @@ const router: Router = Router();
 router.post("/", userRegisterValidation(), Validate, HandleErrors(UserControllers.Register));
 router.post("/login", userLoginValidation(), Validate, HandleErrors(UserControllers.Login));
 router.get("/autologin", Authenticate, HandleErrors(UserControllers.AutoLogin));
-router.get("/verify/:slug", HandleErrors(UserControllers.VerifySlug));
+router.get("/verify/:slug", UserControllers.VerifySlug);
 router.post("/password/", userChangePasswordRequestValidation(), Validate, HandleErrors(UserControllers.ChangePasswordRequest));
 router.patch("/password/", userChangePasswordValidation(), Validate, AuthenticatePassword,HandleErrors(UserControllers.ChangePassword));
 
