b9c10770d2912188dca38e67092065507418809f
Alex Conder <alex.v.conder@gmail.com>
2022-11-08 20:41:44 -0300

    identation

--- src/sockets/Connect/ConnectServices.ts
+++ src/sockets/Connect/ConnectServices.ts
@@ -1,32 +1,28 @@
 import { iOnlineUser } from "../SocketTypes";
 
 export default class ConnectServices {
-  static onlineUsers: iOnlineUser[];
+   static onlineUsers: iOnlineUser[];
 
-  constructor() {
-    ConnectServices.onlineUsers = [] as iOnlineUser[];
-  }
+   constructor() {
+      ConnectServices.onlineUsers = [] as iOnlineUser[];
+   }
 
-  static ConnectUser(userID: string, userName: string, socketID: string) {
-    if (!ConnectServices.onlineUsers.find((user) => user.userID === userID)) {
-      ConnectServices.onlineUsers.push({ userID, userName, socketID });
-    }
-  }
+   static ConnectUser(userID: string, userName: string, socketID: string) {
+      if (!ConnectServices.onlineUsers.find((user) => user.userID === userID)) {
+         ConnectServices.onlineUsers.push({ userID, userName, socketID });
+      }
+   }
 
-  static DisconnectUser(socketID: string) {
-    ConnectServices.onlineUsers = ConnectServices.onlineUsers.filter(
-      (user) => user.socketID !== socketID
-    );
-  }
+   static DisconnectUser(socketID: string) {
+      ConnectServices.onlineUsers = ConnectServices.onlineUsers.filter((user) => user.socketID !== socketID);
+   }
 
-  static GetUser(userID: string) {
-    const user = ConnectServices.onlineUsers.find(
-      (user) => user.userID === userID
-    );
-    if (user) {
-      return user;
-    } else {
-      return false;
-    }
-  }
+   static GetUser(userID: string) {
+      const user = ConnectServices.onlineUsers.find((user) => user.userID === userID);
+      if (user) {
+         return user;
+      } else {
+         return false;
+      }
+   }
 }
--- src/sockets/Notification/NotificationServices.ts
+++ src/sockets/Notification/NotificationServices.ts
@@ -9,36 +9,35 @@ import { iNoficationData } from "../SocketTypes";
 
 import ConnectServices from "../Connect/ConnectServices";
 
+export default class NotificationServices {
+   static async Notify(data: iNoficationData) {
+      try {
+         const { userID, notification } = data;
 
-export default class NotificationServices{
-    static async Notify(data: iNoficationData){
-        try {
-            const { userID, notification } = data;
-            
-            const objectUserID = new ObjectId(String(userID));
-            const currentUser = (await User.findOne({ _id: objectUserID })) as iUser;
-            const notificationList = currentUser?.notifications ? currentUser.notifications : [];
-      
-            const newNotificationList = [...notificationList, notification];
-      
-            await User.updateOne(
-              { _id: objectUserID },
-              {
-                $set: {
+         const objectUserID = new ObjectId(String(userID));
+         const currentUser = (await User.findOne({ _id: objectUserID })) as iUser;
+         const notificationList = currentUser?.notifications ? currentUser.notifications : [];
+
+         const newNotificationList = [...notificationList, notification];
+
+         await User.updateOne(
+            { _id: objectUserID },
+            {
+               $set: {
                   notifications: newNotificationList,
-                },
-              }
-            );
-      
-            const receiver = ConnectServices.GetUser(data.userID);
-      
-            if (receiver) {
-              io.to(receiver.socketID).emit("@GetNotify", {
-                notifications: newNotificationList,
-              });
+               },
             }
-          } catch (error) {
-            console.log(`Ocorreu um erro! Error: ${error}`);
-          }
-    }
+         );
+
+         const receiver = ConnectServices.GetUser(data.userID);
+
+         if (receiver) {
+            io.to(receiver.socketID).emit("@GetNotify", {
+               notifications: newNotificationList,
+            });
+         }
+      } catch (error) {
+         console.log(`Ocorreu um erro! Error: ${error}`);
+      }
+   }
 }
